6a838d678c8a7101f19c4cdb0a56e8c41013876c
pavel <pavels.judakov@inbox.lv>
2022-12-20 22:58:03 +0200

    Varuables

---
+++ src/LessonCode2.java
@@ -0,0 +1,13 @@
+public class LessonCode2 {
+    public static void main(String[] args) {
+
+        Integer age = 35;
+        Double weight = 81.1;
+        Integer a = 10;
+        Integer b = 4;
+        double result = a/ (double) b;
+        System.out.println("age: " + age);
+        System.out.println("weight: " + weight);
+        System.out.println(result);
+    }
+}
